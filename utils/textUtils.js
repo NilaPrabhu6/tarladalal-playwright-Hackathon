8d23438bd3fc9511d1e5dbc1b6d0ecb4f235a76d
NilaPrabhu6 <164823254+NilaPrabhu6@users.noreply.github.com>
2026-06-12 16:36:03 -0400

    Remove text utility functions from textUtils.js

--- utils/textUtils.js
+++ utils/textUtils.js
@@ -1,16 +1,1 @@
-function cleanText(value) {
-  return (value || '').replace(/\s+/g, ' ').trim();
-}
 
-function matchAfter(label, source) {
-  const pattern = new RegExp(`${label}\\s*:?\\s*([^\\n|]+)`, 'i');
-  const match = source.match(pattern);
-  return cleanText(match?.[1] || '');
-}
-
-function extractRecipeId(url) {
-  const match = url.match(/-(\d+)r?\/?$/i) || url.match(/[?&]recipeid=(\d+)/i);
-  return match?.[1] || '';
-}
-
-module.exports = { cleanText, matchAfter, extractRecipeId };
